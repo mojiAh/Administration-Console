d3f5b7427693e7a3f20cbcb6f91093978239449f
mojtaba <mo.ahmadi@outlook.com>
2016-12-26 22:16:28 +0200

    Add basic script file

---
+++ Client/index.module.js
@@ -0,0 +1,8 @@
+angular.module('myApp',[])
+    .controller ('helemt', function ($scope) {
+        $scope.helmets= [
+        {id: '1' , name : 'mojtaba' , location: 'Tampere' , lastUpdate:'12:00'},
+        {id: '2' , name : 'mikko' , location: 'Tampere' , lastUpdate: '12:21'},
+        {id: '3' , name : 'mirja' , location: 'Tampere' , lastUpdate: '12:34'}
+        ];
+});

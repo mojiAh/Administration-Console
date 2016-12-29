9eeee888ece3ef8c83e2f066288e054be53ed157
mojtaba <mo.ahmadi@outlook.com>
2016-12-29 22:12:25 +0200

    Componentize application and seperate everything based on their function

--- Client/index.module.js
+++ Client/index.module.js
@@ -1,8 +1,5 @@
-angular.module('myApp',[])
-    .controller ('helemtController', function HelmetController ($scope) {
-        $scope.helmets= [
-        {id: '1' , name : 'mojtaba' , location: 'Tampere' , lastUpdate:'12:00'},
-        {id: '2' , name : 'mikko' , location: 'Tampere' , lastUpdate: '12:21'},
-        {id: '3' , name : 'mirja' , location: 'Tampere' , lastUpdate: '12:34'}
-        ];
-});
+'use strict';
+
+angular.module('myApp', [
+    'helmetList'
+]);

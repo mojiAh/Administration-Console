be21598349587ed1f1a6424a4b67f668eac2cccd
mojtaba <mo.ahmadi@outlook.com>
2017-01-07 19:04:34 +0200

    add other REST api method for helmet

--- Backend/routes/helmets.js
+++ Backend/routes/helmets.js
@@ -10,8 +10,34 @@ var helmet = require('../models/helmets');
 router.get('/', function(req, res, next) {
     helmet.find(function (err, helmets) {
         if (err) return next(err);
-        console.log(helmets);
         res.json(helmets);
     });
 });
+
+
+/* POST /helmets */
+router.post('/', function(req, res, next) {
+    helmet.create(req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next) {
+    helmet.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+/* DELETE /todos/:id */
+router.delete('/:id', function(req, res, next) {
+    helmet.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });
+});
+
+
 module.exports = router;

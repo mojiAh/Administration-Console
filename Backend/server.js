0c27d3234257d4a5810f5f96d6189cdd31e6137b
mojtaba <mo.ahmadi@outlook.com>
2017-01-07 13:10:59 +0200

    optimize server structure

--- Backend/server.js
+++ Backend/server.js
@@ -3,18 +3,18 @@ var app = express();
 
 // Load mongoose package
 var mongoose = require('mongoose');
+
 // Connect to MongoDB and create/use database called SmartHardHat
 mongoose.connect('mongodb://localhost/smartHardHat');
 
-var helmets = require('./Backend/routes/helmets');
+var helmets = require('./routes/helmets');
 app.use('/helmetList', helmets);
 
 // make server port
 var PORT= process.env.PORT || 3000;
 
 // this middleware load our static files like CSS, JS, HTML to our server
-app.use(express.static(__dirname + "/Client"));
-
+app.use(express.static(__dirname + "/../Client"));
 
 
 //listening to port
